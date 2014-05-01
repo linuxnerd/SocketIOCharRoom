c7620150b44a8823921f4bda066df413ec0fcc88
Eric Lou <lwc_evale@hotmail.com>
2014-05-01 23:32:04 +0800

    change server address

--- js/client_config.js
+++ js/client_config.js
@@ -1,7 +1,7 @@
 (function (root){
     root.chat_config = {
         protocol : "http://", // 协议
-        remote_ip : "tipschat.herokuapp.com", // 地址
-        remote_port : 80 // 端口
+        remote_ip : "piaofang.newnil.com", // 地址
+        remote_port : 9000 // 端口
     }
 })(window);
