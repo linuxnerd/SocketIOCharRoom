796cb298d04d96c102812ee6d25f6d6d05028b62
sunday35034 <lwc_evale@hotmail.com>
2014-05-05 17:48:27 +0800

    modify port

--- js/client_config.js
+++ js/client_config.js
@@ -1,7 +1,7 @@
 (function (root){
     root.chat_config = {
         protocol : "http://", // 协议
-        remote_ip : "tipschat.herokuapp.com", // 地址
-        remote_port : 80 // 端口
+        remote_ip : "172.16.27.29", // 地址
+        remote_port : 5000 // 端口
     }
 })(window);
